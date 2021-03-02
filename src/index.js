ea8671a80a3792bd85193ef027001a7c2da00da1
QuiteAir <hfvbkmr@mail.ru>
2021-03-02 04:06:17 +0300

    feat: implement function toReadable that converts given number, to readable string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function toReadable (number) {
-  
+  let result = number ? '' : 'zero';
+  let wS = number;
+  let numBefore = false;
+  let lib = [
+    ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+  'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
+
+  while (wS > 0) {
+    if (wS < 20) {
+      if (numBefore) {
+        result += ' ';
+      }
+      result += lib[0][wS];
+      wS -= wS;
+    }
+
+    else if (wS < 100) {
+      for (let i = 9; i > 0; i--) {
+        if ((wS % (i * 10)) < 10) {
+          if (numBefore) {
+            result += ' ';
+          }
+          result += lib[i - 1];
+          numBefore = true;
+          wS = wS - (i * 10);
+          i -= 10; //exit loop
+        }
+      }
+    }
+
+    else if (wS < 1000) {
+      for (let i = 9; i > 0; i--) {
+        if ((wS % (i * 100)) < 100) {
+          result += lib[0][i] + ' hundred'
+          numBefore = true;
+          wS = wS - (i * 100);
+          i -= 10; //exit loop
+        }
+      }
+    }
+    else {
+      result = 'err'
+    }
+  }
+  return result;
 }
